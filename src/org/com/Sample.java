df042ecf0c95f96b2993ff92a4ba88fa5b97ecf2
maheshwari1026 <146749774+maheshwari1026@users.noreply.github.com>
2023-11-16 19:11:35 +0530

    add few lines

---
+++ src/org/com/Sample.java
@@ -0,0 +1,15 @@
+package org.com;
+
+public class Sample {
+		
+		public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		for (int i = 0; i < 10; i++) {
+		System.out.println(i);	
+		}
+	}
+
+	}
+
